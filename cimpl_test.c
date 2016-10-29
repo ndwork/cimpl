30fcac81d20eb2014120085ce08c8af5fcf43524
ndwork <ndwork@Nicholass-MacBook-Pro-5.local>
2016-10-29 14:46:42 -0700

    Added a couple more test routines.

--- cimpl_test.c
+++ cimpl_test.c
@@ -3,7 +3,7 @@
 //  cimpl
 //
 //  Created by Nicholas Dwork on 10/29/16.
-//  Copyright © 2016 Nicholas Dwork. All rights reserved.
+//  Copyright © 2016 Nicholas Dwork.
 //
 
 #include <math.h>
@@ -110,6 +110,7 @@ int test_divideImgByScalar(){
   cimpl_imgf myImg, result, answer;
   unsigned int M=2;
   unsigned int N=3;
+  float scalar = 2;
   unsigned int out;
   
   myImg = cimpl_mallocImg(M, N);
@@ -117,15 +118,86 @@ int test_divideImgByScalar(){
   answer = cimpl_mallocImg(M, N);
   
   for( unsigned int i=1; i<=M*N; ++i ){
-    myImg.data[i-1] = 2*i;
+    myImg.data[i-1] = scalar * i;
     answer.data[i-1] = i;
   }
-  cimpl_divideImgByScalar(myImg, 2, &result);
+  cimpl_divideImgByScalar(myImg, scalar, &result);
   out = cimpl_equalImgs(result, answer);
   
   cimpl_freeImg(&myImg);
   cimpl_freeImg(&result);
   cimpl_freeImg(&answer);
+  return out;
+}
+
+int test_multiplyImgs(){
+  cimpl_imgf img1, img2, result, answer;
+  unsigned int M=2;
+  unsigned int N=3;
+  unsigned int out;
+  
+  img1 = cimpl_mallocImg(M, N);
+  img2 = cimpl_mallocImg(M, N);
+  result = cimpl_mallocImg(M, N);
+  answer = cimpl_mallocImg(M, N);
+  
+  for( unsigned int i=0; i<M*N; ++i ){
+    img1.data[i] = i;
+    img2.data[i] = i+1;
+    answer.data[i] = i*(i+1);
+  }
+  cimpl_multiplyImgs(img1, img2, &result);
+  out = cimpl_equalImgs(result, answer);
+  
+  cimpl_freeImg(&img1);
+  cimpl_freeImg(&img2);
+  cimpl_freeImg(&result);
+  cimpl_freeImg(&answer);
+  return out;
+}
+
+int test_multiplyImgByScalar(){
+  cimpl_imgf myImg, result, answer;
+  unsigned int M=2;
+  unsigned int N=3;
+  float scalar = 1.8;
+  unsigned int out;
+
+  myImg = cimpl_mallocImg(M, N);
+  result = cimpl_mallocImg(M, N);
+  answer = cimpl_mallocImg(M, N);
+
+  for( unsigned int i=1; i<=M*N; ++i ){
+    myImg.data[i-1] = i;
+    answer.data[i-1] = scalar * i;
+  }
+  cimpl_multiplyImgByScalar(myImg, scalar, &result);
+  out = cimpl_equalImgs(result, answer);
+  
+  cimpl_freeImg(&myImg);
+  cimpl_freeImg(&result);
+  cimpl_freeImg(&answer);
+  return out;
+}
+
+int test_sumImg(){
+  cimpl_imgf myImg;
+  float result, answer;
+  unsigned int M=2;
+  unsigned int N=3;
+  unsigned int out;
+  
+  myImg = cimpl_mallocImg(M,N);
+
+  answer = 0;
+  for( unsigned int i=0; i<M*N; ++i ){
+    myImg.data[i] = (float) i/3;
+    answer += (float) i/3;
+  }
+  result = cimpl_sumImg(&myImg);
+  out = ( result == answer );
+  
+  cimpl_freeImg(&myImg);
   return out;
 }
 
@@ -156,10 +228,23 @@ void cimpl_test(){
   } else {
     printf("failed - cimpl_divideImgs\n");
   }
-  
+
   if( test_divideImgByScalar() ){
     printf("passed - cimpl_divideImgByScalar\n");
   } else {
     printf("failed - cimpl_divideImgByScalar\n");
   }
+
+  if( test_multiplyImgs() ){
+    printf("passed - cimpl_multiplyImgs\n");
+  } else {
+    printf("failed - cimpl_multiplyImgs\n");
+  }
+  
+  if( test_sumImg() ){
+    printf("passed - cimpl_sumImg\n");
+  } else {
+    printf("failed - cimpl_sumImg\n");
+  }
+  
 }
