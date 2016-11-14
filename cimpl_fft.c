328809000eb3b2399238a0db2a67026c1038e8ff
ndwork <ndwork@Nicholass-MacBook-Pro-5.local>
2016-11-13 23:39:27 -0800

    Fixed a bug; fftw_ changed to fftwf_

--- cimpl_fft.c
+++ cimpl_fft.c
@@ -17,37 +17,37 @@
 #include <fftw3.h>
 
 void cimpl_prefftCmpImg( cimpl_cmpImg const fftIn, cimpl_cmpImg const fftOut, void* out ){
-  fftw_plan fftPlan;
-  fftPlan = fftw_plan_dft_2d( (int) fftIn.h, (int) fftIn.w,
-    (fftw_complex*) fftIn.data, (fftw_complex*) fftOut.data,
+  fftwf_plan fftPlan;
+  fftPlan = fftwf_plan_dft_2d( (int) fftIn.h, (int) fftIn.w,
+    (fftwf_complex*) fftIn.data, (fftwf_complex*) fftOut.data,
     FFTW_FORWARD, FFTW_MEASURE );
 }
 
 void cimpl_prefftCmpVol( cimpl_cmpVol const fftIn, cimpl_cmpVol const fftOut, void* out ){
-  fftw_plan fftPlan;
-  fftPlan = fftw_plan_dft_3d( (int) fftIn.h, (int) fftIn.w, (int) fftIn.s,
-    (fftw_complex*) fftIn.data, (fftw_complex*) fftOut.data,
+  fftwf_plan fftPlan;
+  fftPlan = fftwf_plan_dft_3d( (int) fftIn.h, (int) fftIn.w, (int) fftIn.s,
+    (fftwf_complex*) fftIn.data, (fftwf_complex*) fftOut.data,
     FFTW_FORWARD, FFTW_MEASURE );
 }
 
 void cimpl_fftCmpImg( void* in ){
-  fftw_plan* tmp = in;
-  fftw_execute( *tmp );
+  fftwf_plan* tmp = in;
+  fftwf_execute( *tmp );
 }
 
 void cimpl_fftCmpVol( void* in ){
-  fftw_plan* tmp = in;
-  fftw_execute( *tmp );
+  fftwf_plan* tmp = in;
+  fftwf_execute( *tmp );
 }
 
 void cimpl_postfftCmpImg( void* in ){
-  fftw_plan* tmp = in;
-  fftw_destroy_plan( *tmp );
+  fftwf_plan* tmp = in;
+  fftwf_destroy_plan( *tmp );
 }
 
 void cimpl_postfftCmpVol( void* in ){
-  fftw_plan* tmp = in;
-  fftw_destroy_plan( *tmp );
+  fftwf_plan* tmp = in;
+  fftwf_destroy_plan( *tmp );
 }
 
 
